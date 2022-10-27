b4a0273947749ba092ea547639ac0aa3d67707fe
RianYuri <rianyurirgt@gmail.com>
2022-10-27 00:05:11 -0300

    V2.2

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,5 +1,4 @@
 
-
 let textsArray;
 await fetch('../index.xml')
 .then(response => response.text())
@@ -10,13 +9,8 @@ await fetch('../index.xml')
     let texts = xmlDOM.querySelectorAll('lbl');
     textsArray = texts
 })
-
-/* 
-Selecionando todos os elementos que estão dentro da tag com id xml
-e possuem o atributo data-text 
-*/
 let divsXml = document.querySelectorAll('#xml [data-text]')
-/* Adicionando o valor do elemento se o data-text for igual ao elemento no xml */
+
 divsXml.forEach((item, index) =>{
     if(item.dataset.text == textsArray[index].attributes[0].value){
          item.innerHTML = textsArray[index].innerHTML
